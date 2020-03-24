ecdfde14531f6a50c9ff6f9aa2db28a972998fee
tjw <tjw@data-sci.info>
2020-03-24 17:56:42 +0800

    add hello examples and thread examples

---
+++ 01-hello/hello_cuda_unmanaged.cu
@@ -0,0 +1,55 @@
+/*
+code from https://devblogs.nvidia.com/even-easier-introduction-cuda/
+nvcc -o hello_cuda_unmanaged hello_cuda_unmanaged.cu 
+*/
+#include <iostream>
+#include <math.h>
+// Kernel function to add the elements of two arrays
+__global__
+void add(int n, float *x, float *y)
+{
+  for (int i = 0; i < n; i++)
+    y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1<<20;
+  float *x = new float[N];
+  float *y = new float[N];
+  float *d_x, *d_y; // device copies of x,y
+
+  // Allocate GPU Memory
+  cudaMalloc(&d_x, N*sizeof(float));
+  cudaMalloc(&d_y, N*sizeof(float));
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+  // Copy from host to the device
+  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+
+  // Run kernel on 1M elements on the GPU
+  add<<<1, 1>>>(N, d_x, d_y);
+
+  // Copy result from device to host
+  // cudaDeviceSychronize() is done in cudaMemcpy
+  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+  delete [] x;
+  delete [] y;
+  
+  return 0;
+}
